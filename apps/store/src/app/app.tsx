2905ff9b885bcc17eab3ebeb5adab25fe114e9ed
Ken Ling <ken.lingyh@hendrickscorp.com>
2022-01-17 16:16:54 +0800

    @emotion/react
    global setting css

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -1,5 +1,7 @@
 import styled from '@emotion/styled';
 import { Route, Link } from 'react-router-dom';
+import { Global, css } from '@emotion/react';
+
 import { getAllGames } from '../fake-api';
 import Card from '@material-ui/core/Card';
 import CardActionArea from '@material-ui/core/CardActionArea';
@@ -7,49 +9,62 @@ import CardContent from '@material-ui/core/CardContent';
 import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 
-const StyledApp = styled.div`
-  // Your style here
+const color = 'white';
+
+const StyledApp = styled.div``;
+
+const styles = css`
+  .container {
+    //background-color: blue;
+  }
+  .game-card {
+    display: flex;
+    border: 1px solid red;
+  }
 `;
 
 export function App() {
   return (
-    <StyledApp>
-      <div className="container">
-        <div className="games-layout">
-          {getAllGames().map((x) => (
-            <Card key={x.id} className="game-card">
-              <CardActionArea>
-                <CardMedia
-                  className="game-card-media"
-                  image={x.image}
-                  title={x.name}
-                />
-                <CardContent>
-                  <Typography gutterBottom variant="h5" component="h2">
-                    {x.name}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                  >
-                    {x.description}
-                  </Typography>
-                  <Typography
-                    variant="body2"
-                    color="textSecondary"
-                    component="p"
-                    className="game-rating"
-                  >
-                    <strong>Rating:</strong> {x.rating}
-                  </Typography>
-                </CardContent>
-              </CardActionArea>
-            </Card>
-          ))}
+    <>
+      <Global styles={styles} />
+      <div>
+        <div className="container">
+          <div className="games-layout">
+            {getAllGames().map((x) => (
+              <Card key={x.id} className="game-card">
+                <CardActionArea>
+                  <CardMedia
+                    className="game-card-media"
+                    image={x.image}
+                    title={x.name}
+                  />
+                  <CardContent>
+                    <Typography gutterBottom variant="h5" component="h2">
+                      {x.name}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                    >
+                      {x.description}
+                    </Typography>
+                    <Typography
+                      variant="body2"
+                      color="textSecondary"
+                      component="p"
+                      className="game-rating"
+                    >
+                      <strong>Rating:</strong> {x.rating}
+                    </Typography>
+                  </CardContent>
+                </CardActionArea>
+              </Card>
+            ))}
+          </div>
         </div>
       </div>
-    </StyledApp>
+    </>
   );
 }
 
